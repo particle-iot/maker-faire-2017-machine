d1dfce04cc5d27e56e75f1d6b693b39d6d5ee0cb
Julien Vanier <julien@particle.io>
2017-04-18 03:49:38 -0400

    Add lighting module code

---
+++ Lighting/src/lighting.ino
@@ -0,0 +1,96 @@
+/*
+ * Lighting behind the interaction surface
+ */
+
+
+#include "neopixel.h"
+#include "mf2017-can.h"
+
+SYSTEM_THREAD(ENABLED);
+
+Communication comms;
+
+// Neopixel strip
+#define PIXEL_PIN A1
+//#define PIXEL_COUNT 183
+#define PIXEL_COUNT 56
+#define PIXEL_TYPE WS2812B
+
+Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+
+// Brightness
+#define BRIGHTNESS_PIN A0
+
+void setup() {
+  Serial.begin();
+  comms.begin();
+  strip.begin();
+  strip.show();
+  startAttractMode();
+}
+
+void loop() {
+  comms.receive();
+  updateAttractMode();
+  updateBrightness();
+  display();
+  delay(10);
+}
+
+enum {
+  ATTRACT_MODE
+} state = ATTRACT_MODE;
+
+uint8_t attract[PIXEL_COUNT];
+int attractPos;
+int attractCount;
+
+void startAttractMode() {
+  for (int i = 0; i < PIXEL_COUNT; i++) {
+    attract[i] = 0;
+  }
+  attractPos = 0;
+  attractCount = 0;
+}
+
+void updateAttractMode() {
+  if (attractCount >= PIXEL_COUNT) {
+    Serial.println("restarting");
+    startAttractMode();
+  }
+
+  Serial.printlnf("Update pos=%d count=%d", attractPos, attractCount);
+  attract[attractPos] = 1;
+  if (attractPos != 0) {
+    attract[attractPos - 1] = 0;
+  }
+  attractPos++;
+
+  if (attractPos >= PIXEL_COUNT - attractCount) {
+    Serial.println("Starting next round");
+    attractPos = 0;
+    attractCount++;
+  }
+}
+
+void updateBrightness() {
+  int brightness = analogRead(BRIGHTNESS_PIN) / 16 + 1;
+  if (brightness > 255) {
+    brightness = 255;
+  }
+  strip.setBrightness(brightness);
+}
+
+#define ATTRACT_COLOR 0xffffff
+void display() {
+  switch (state) {
+    case ATTRACT_MODE:
+      Serial.println("Displaying");
+      for (int i = 0; i < PIXEL_COUNT; i++) {
+        strip.setPixelColor(i, attract[i] ? ATTRACT_COLOR : 0);
+      }
+      break;
+  }
+  strip.show();
+}
+
