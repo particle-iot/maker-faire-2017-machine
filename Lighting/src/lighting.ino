ecb0a4fe783eb28b2262467fef584b238fa06aba
Julien Vanier <julien@particle.io>
2017-04-23 16:20:53 -0400

    Add active pattern for panel 2

--- Lighting/src/lighting.ino
+++ Lighting/src/lighting.ino
@@ -5,6 +5,7 @@
 
 #include "neopixel.h"
 #include "mf2017-can.h"
+#include "hsv.h"
 #include "simple-storage.h"
 #include <algorithm>
 using namespace std;
@@ -15,33 +16,39 @@ BuiltinCAN can(CAN_D1_D2);
 Communication comms(can);
 
 // Neopixel strip
-#define PIXEL_PIN A1
+const auto PIXEL_PIN = A1;
 //#define PIXEL_COUNT 183
-#define PIXEL_COUNT 56
-#define PIXEL_TYPE WS2812B
+const auto PIXEL_COUNT = 56;
+const auto PIXEL_TYPE = WS2812B;
 
 Adafruit_NeoPixel strip(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
 // Brightness
-#define BRIGHTNESS_PIN A0
+const auto BRIGHTNESS_PIN = A0;
 
 // Config stored in EEPROM
-#define PANEL_COUNT 4
+const auto PANEL_COUNT = 4;
 struct Config {
   uint32_t app;
   uint8_t fillingDelay;
   uint8_t theaterChaseDelay;
+  uint8_t theaterChaseDuration;
 
   uint8_t panelFirst[PANEL_COUNT];
   uint8_t panelSize[PANEL_COUNT];
+  uint8_t activeTimeout;
+  uint8_t colorFillDelay;
 } config;
 
 Config defaultConfig = {
-  /* app */ APP_CODE('M', 'F', 'L', 0),
-  /* fillingDelay */ 10,
-  /* theaterChaseDelay */ 250,
+  /* app */ APP_CODE('M', 'F', 'L', 2),
+  /* fillingDelay */ 20,
+  /* theaterChaseDelay */ 80,
+  /* theaterChaseDuration */ 8,
   /* panelFirst */ { 0, 14, 28, 42 },
   /* panelCount */ { 14, 14, 14, 14 },
+  /* activeTimeout */ 30,
+  /* colorFillDelay */ 10,
 };
 
 Storage<Config> storage(defaultConfig);
@@ -53,7 +60,7 @@ void setup() {
   comms.begin();
   strip.begin();
   strip.show();
-  startAttractMode();
+  startAttractFilling();
 }
 
 void loop() {
@@ -61,8 +68,11 @@ void loop() {
   comms.receive();
   updateAttractMode();
   updateBrightness();
+
+  // FIXME updatePanel1();
+  updatePanel2();
+
   display();
-  delay(10);
 }
 
 void registerCloud() {
@@ -74,15 +84,18 @@ void registerCloud() {
 }
 
 /* Attract mode will fill the lights at the bottom of inactive panels
- * one dot at a time, then do a theater chase */
+ * one dot at a time, then do a theater chase
+ */
 enum {
   FILLING,
   THEATER_CHASE
 } attractState = FILLING;
 
 uint8_t attract[PIXEL_COUNT];
-int attractPos;
-int attractCount;
+unsigned attractPos;
+unsigned attractCount;
+long attractLastUpdate;
+long attractStart;
 
 uint8_t attractSize() {
   uint8_t s = 0;
@@ -92,19 +105,44 @@ uint8_t attractSize() {
   return s;
 }
 
-void startAttractMode() {
+void startAttractTheaterChase() {
+  clearAttract();
+  attractState = THEATER_CHASE;
+}
+
+void startAttractFilling() {
+  clearAttract();
+  attractState = FILLING;
+}
+
+void clearAttract() {
   for (unsigned i = 0, s = attractSize(); i < s; i++) {
     attract[i] = 0;
   }
   attractPos = 0;
   attractCount = 0;
+  attractLastUpdate = 0;
+  attractStart = millis();
 }
 
 void updateAttractMode() {
-  unsigned s = attractSize();
-  if (attractCount >= s) {
-    startAttractMode();
+  switch (attractState) {
+    case FILLING:
+      updateAttractFilling();
+      break;
+    case THEATER_CHASE:
+      updateAttractTheaterChase();
+      break;
   }
+}
+
+void updateAttractFilling() {
+  if (millis() - attractLastUpdate < config.fillingDelay) {
+    return;
+  }
+  attractLastUpdate = millis();
+
+  unsigned s = attractSize();
 
   attract[attractPos] = 1;
   if (attractPos != 0) {
@@ -116,29 +154,158 @@ void updateAttractMode() {
     attractPos = 0;
     attractCount++;
   }
+
+  if (attractCount >= s) {
+    startAttractTheaterChase();
+  }
+}
+
+void updateAttractTheaterChase() {
+  if (millis() - attractLastUpdate < config.theaterChaseDelay) {
+    return;
+  }
+  attractLastUpdate = millis();
+
+  for (unsigned i = 0, s = attractSize(); i < s; i++) {
+    unsigned n = (i - attractCount) % 4;
+    attract[i] = (n < 2) ? 1 : 0;
+  }
+  attractCount++;
+
+  if (millis() - attractStart > config.theaterChaseDuration * 1000) {
+    startAttractFilling();
+  }
+}
+
+/* Are panels being interacted with?
+ */
+enum {
+  PANEL1,
+  PANEL2,
+  PANEL3,
+  PANEL4
+};
+bool panelActive[PANEL_COUNT] = { 0 };
+long panelLastActive[PANEL_COUNT] = { 0 };
+uint32_t panelPixels[PIXEL_COUNT] = { 0 };
+
+void clearPanel(unsigned panel) {
+  unsigned first = config.panelFirst[panel];
+  for (unsigned i = first, n = first + config.panelSize[panel]; i < n; i++) {
+    panelPixels[i] = 0;
+  }
+}
+
+/* Panel 1 interaction: Color fill with the color of the button pressed
+ * then breathe
+ */
+
+const auto RED_COLOR = 0xff0000;
+const auto GREEN_COLOR = 0x00ff00;
+const auto BLUE_COLOR = 0x0000ff;
+
+unsigned panel1Count;
+uint32_t panel1Color;
+
+void updatePanel1() {
+  if (comms.Input1Active && !panelActive[PANEL1]) {
+    panelActive[PANEL1] = true;
+    if (comms.RedButtonPressed) {
+      panel1Color = RED_COLOR;
+    } else if (comms.GreenButtonPressed) {
+      panel1Color = GREEN_COLOR;
+    } else if (comms.BlueButtonPressed) {
+      panel1Color = BLUE_COLOR;
+    }
+  }
+}
+
+/* Panel 2 interaction: flow current color
+ */
+
+long panel2LastUpdate = 0;
+
+void updatePanel2() {
+  if (comms.Input2Active) {
+    // panel just became active
+    if (!panelActive[PANEL2]) {
+      panelActive[PANEL2] = true;
+      clearPanel(PANEL2);
+    }
+    panelLastActive[PANEL2] = millis();
+  }
+
+  if (!panelActive[PANEL2]) {
+    return;
+  }
+
+  if (millis() - panel2LastUpdate < config.colorFillDelay) {
+    return;
+  }
+  panel2LastUpdate = millis();
+
+  // FIXME: HACK!
+  unsigned x = analogRead(BRIGHTNESS_PIN) / 16 + 1;
+  if (x > 255) {
+    x = 255;
+  }
+  comms.InputColorHue = x;
+
+  // Color flow
+  if (panelActive[PANEL2]) {
+    // shift every pixel one forward
+    unsigned first = config.panelFirst[PANEL2];
+    for (unsigned i = first + config.panelSize[PANEL2] - 1; i > first; i--) {
+      panelPixels[i] = panelPixels[i - 1];
+    }
+
+    // Compute RGB of input hue
+    RgbColor inputColor = HsvToRgb(HsvColor(comms.InputColorHue, 255, 255));
+    uint32_t c = strip.Color(inputColor.r, inputColor.g, inputColor.b);
+    panelPixels[first] = c;
+  }
+
+  if (millis() - panelLastActive[PANEL2] > config.activeTimeout * 1000) {
+    panelActive[PANEL2] = false;
+  }
 }
 
 void updateBrightness() {
-  int brightness = analogRead(BRIGHTNESS_PIN) / 16 + 1;
-  if (brightness > 255) {
-    brightness = 255;
-  }
+  //unsigned brightness = analogRead(BRIGHTNESS_PIN) / 16 + 1;
+  //if (brightness > 255) {
+  //  brightness = 255;
+  //}
+  unsigned brightness = 16;
   strip.setBrightness(brightness);
 }
 
-#define ATTRACT_COLOR 0xffffff
-const uint32_t attractColors[] = {
-  0xffffff,
-  0xff0000,
-  0x00ff00,
-  0x0000ff
-};
+const auto ATTRACT_COLOR = 0xffffff;
+
+long displayLastUpdate = 0;
+const auto DISPLAY_INTERVAL = 10;
 
 void display() {
+  if (millis() - displayLastUpdate < DISPLAY_INTERVAL) {
+    return;
+  }
+  displayLastUpdate = millis();
+
   for (unsigned panel = 0; panel < PANEL_COUNT; panel++) {
-    displayAttract(panel);
+    if (panelActive[panel]) {
+      displayActive(panel);
+    } else {
+      displayAttract(panel);
+    }
   }
   strip.show();
+}
+
+// Fill the lights of panel with the current active mode pattern
+void displayActive(unsigned panel) {
+  unsigned first = config.panelFirst[panel];
+  for (unsigned i = first, n = first + config.panelSize[panel]; i < n; i++) {
+    strip.setPixelColor(i, panelPixels[i]);
+  }
 }
 
 // Fill the lights of panel with the current attract mode pattern
@@ -164,6 +331,12 @@ int updateConfig(String arg) {
         config.fillingDelay = value.toInt();
     } else if (name.equals("theaterChaseDelay")) {
         config.theaterChaseDelay = value.toInt();
+    } else if (name.equals("theaterChaseDuration")) {
+        config.theaterChaseDuration = value.toInt();
+    } else if (name.equals("activeTimeout")) {
+        config.activeTimeout = value.toInt();
+    } else if (name.equals("colorFillDelay")) {
+        config.colorFillDelay = value.toInt();
     } else {
         return -2;
     }
